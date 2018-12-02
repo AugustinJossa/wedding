a1f43c578387660328774182ac284009f8820038
Augustin Jossa <augustin.jossa@hec.edu>
2018-12-02 21:12:01 +0100

    movies ok

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,15 @@ skip_before_action :authenticate_user!, only: [:new, :create]
 
   def create
     @movie = Movie .new(movie_params)
-    if @movie.save
+    params[:movie][:url][0..47]
+
+    if params[:movie][:url][0..47] == "http://www.allocine.fr/film/fichefilm_gen_cfilm=" || params[:movie][:url] == "https://www.allocine.fr/film/fichefilm_gen_cfilm"
+      @movie.save
       redirect_to extra_path, notice: "Le film a bien été ajouté"
     else
-      render :new
+      redirect_to extra_path, alert: "Attention à bien ajouter une fiche de film allocine"
     end
+
   end
 
   private
