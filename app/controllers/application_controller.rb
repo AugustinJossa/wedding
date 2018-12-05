56f7f104e2abacfc0757c0d1deb0f789da067f25
Augustin Jossa <augustin.jossa@hec.edu>
2018-12-05 08:58:50 +0100

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def default_url_options
+    { host: ENV["HOST=www.soleneetaugustin.com"] || "localhost:3000" }
+  end
+
   def logistique
     @user = current_user
     @hotels_bp = Hotel.where(category: "bon plans")
